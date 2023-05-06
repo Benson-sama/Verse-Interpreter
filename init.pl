6bdcd42cc020d807a44cb5b6172429a2ec0e9b2d
Benjamin BOGNER <BOGNER.Benjamin@outlook.at>
2023-05-06 13:27:52 +0200

    Renamed usings, fixed tokenise_file.

--- init.pl
+++ init.pl
@@ -1,4 +1,5 @@
 :- use_module(library(pure_input)).
 :- use_module(library(clpfd)).  % Constraint Logic Programming over Finite Domains.
+:- use_module(library(readutil)).
 
 :- set_prolog_flag(double_quotes, chars).   % Makes strings and list of characters equal.
--- main.pl
+++ main.pl
@@ -1,51 +1,28 @@
-:- [usings, syntax, desugaring, rewriting, tests].
+:- [init, syntax, desugaring, rewriting, tests].
 
 not_implemented :-
     writeln('Not implemented yet.'),
     false.
 
-interpret(VC, Result) :-
+interpret(Verse, Result) :-
     not_implemented,
-    desugar(VC, D),         % TODO.
-    tokenise(D, Ts),
+    desugar(Verse, VC),         % TODO.
+    tokenise(VC, Ts),
     rewrite(Ts, Result).    % TODO.
 
-desugar(VC, VC) :-
-    not_implemented,
-    \+ phrase(desugar(_), VC).
-desugar(VC, Result) :-
-    not_implemented,
-    phrase(desugar(Result1), VC),
-    desugar(Result1, Result).
-
-read_file(File, Content) :-
-    setup_call_cleanup(open(File, read, In),
-        read_string(In, _, Content),
-        close(In)).
-
-file_lines(File, Lines) :-
-    setup_call_cleanup(open(File, read, In),
-        stream_lines(In, Lines),
-        close(In)).
-
-stream_lines(In, Lines) :-
-    read_string(In, _, Str),
-    split_string(Str, "\n", "", Lines).
+% desugar(VC, VC) :-
+%     not_implemented,
+%     \+ phrase(desugar(_), VC).
+% desugar(VC, Result) :-
+%     not_implemented,
+%     phrase(desugar(Result1), VC),
+%     desugar(Result1, Result).
 
 tokenise(Cs, Ts) :- phrase(p(Ts), Cs).
 
 tokenise_file(File, Ts) :-
-    phrase_from_file(p(Ts), File).
+    read_file_to_string(File, S, []),
+    string_chars(S, Cs),
+    phrase(p(Ts), Cs).
 
-tokenise(Ts) :- tokenise_file("samples/main.verse", Ts).
-
-% Sample queries:
-
-% tokenise("one{Ex. Ey. x=5; y=3; 10}", Ts).           
-% Ts = one(exists(x, exists(y, eqe(eq(variable(x), hnf(integer(5))),
-% eqe(eq(variable(y), hnf(integer(3))), hnf(integer(10))))))) .
-
-% tokenise("one{Ex. Ey. Ez. x=5; y=3; z=x|y; z}", Ts).
-% Ts = one(exists(x, exists(y, exists(z, eqe(eq(variable(x), hnf(integer(5))),
-% eqe(eq(variable(y), hnf(integer(3))), eqe(eq(variable(z), choice(variable(...),
-% variable(...))), variable(z)))))))) .
+tokenise(Ts) :- tokenise_file('samples/main.verse', Ts).
