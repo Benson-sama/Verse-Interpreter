ab51d13ee2f26f057981c48883467bfe9f76dd73
Benjamin BOGNER <BOGNER.Benjamin@outlook.at>
2023-05-08 11:43:48 +0200

    Removed desugaring file.

--- main.pl
+++ main.pl
@@ -1,14 +1,12 @@
-:- [init, syntax, desugaring, rewriting, tests].
+:- [init, syntax, rewriting, tests].
 
 not_implemented :-
     writeln('Not implemented yet.'),
     false.
 
 interpret(Verse, Result) :-
-    not_implemented,
-    desugar(Verse, VC),         % TODO.
-    tokenise(VC, Ts),
-    rewrite(Ts, Result).    % TODO.
+    tokenise(Verse, Ts),
+    rewrite(Ts, Result).
 
 % desugar(VC, VC) :-
 %     not_implemented,
@@ -20,9 +18,9 @@ interpret(Verse, Result) :-
 
 tokenise(Cs, Ts) :- phrase(p(Ts), Cs).
 
+tokenise(Ts) :- tokenise_file('samples/main.verse', Ts).
+
 tokenise_file(File, Ts) :-
     read_file_to_string(File, S, []),
     string_chars(S, Cs),
     phrase(p(Ts), Cs).
-
-tokenise(Ts) :- tokenise_file('samples/main.verse', Ts).
--- tests.pl
+++ tests.pl
@@ -2,24 +2,22 @@
 % Therefore this is not working anymore.
 
 test_all :-
-    test_desugar_add,
-    test_desugar_gt,
-    test_desugar_variable_initialisation.
+    test_desugar_add.
 
 test_desugar_add :-
     % Act
-    phrase(desugar_add(X), "one{fail+fail}", []),
+    phrase(p(X), "one{fail+fail}", []),
     % Assert
-    X="one{add<fail, fail>}".
+    X=one(e(application(add, tuple(e(fail), e(fail))))).
 
 test_desugar_gt :-
     % Act
-    phrase(desugar_gt(X), "one{fail>fail}", []),
+    phrase(p(X), "one{fail>fail}", []),
     % Assert
     X="one{gt<fail, fail>}".
 
 test_desugar_variable_initialisation :-
     % Act
-    phrase(desugar_variable_initialisation(X), "one{x := fail; fail}", []),
+    phrase(p(X), "one{x := fail; fail}", []),
     % Assert
     X="one{Ex. x=fail; fail}".
