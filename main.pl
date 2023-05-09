37a17773c7b1d7b1e93480290b449e8ff1aa6cd6
Benjamin BOGNER <BOGNER.Benjamin@outlook.at>
2023-05-09 17:42:41 +0200

    Added printing input when tokenising.

--- main.pl
+++ main.pl
@@ -1,5 +1,6 @@
 :- [init, syntax, rewriting, tests].
 
+% Development predicate.
 not_implemented :-
     writeln('Not implemented yet.'),
     false.
@@ -8,19 +9,13 @@ interpret(Verse, Result) :-
     tokenise(Verse, Ts),
     rewrite(Ts, Result).
 
-% desugar(VC, VC) :-
-%     not_implemented,
-%     \+ phrase(desugar(_), VC).
-% desugar(VC, Result) :-
-%     not_implemented,
-%     phrase(desugar(Result1), VC),
-%     desugar(Result1, Result).
-
 tokenise(Cs, Ts) :- phrase(p(Ts), Cs).
 
 tokenise(Ts) :- tokenise_file('samples/main.verse', Ts).
 
 tokenise_file(File, Ts) :-
     read_file_to_string(File, S, []),
+    write('Tokenising: '),
+    writeln(S),
     string_chars(S, Cs),
     phrase(p(Ts), Cs).
