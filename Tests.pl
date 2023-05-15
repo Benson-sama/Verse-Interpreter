ede7aec8db159524518940b63c0d0a56f55df884
Benson-sama <BOGNER.Benjamin@outlook.at>
2023-05-15 10:52:33 +0200

    Updated test cases.

--- Tests.pl
+++ Tests.pl
@@ -1,22 +1,35 @@
 test_all :-
-    test_desugar_add,
-    test_desugar_gt,
+    test_app_add,
+    test_app_gt,
     test_desugar_variable_initialisation.
 
-test_desugar_add :-
-    % Act
-    phrase(p(X), "fail+fail", []),
-    % Assert
-    X=one(e(application(add, tuple(e(fail), e(fail))))).
+test_app_add :-
+    phrase(p(X), "10+20"),
+    phrase(p(X), "add <10, 20>"),
+    X = one(
+        e(application(
+            v(hnf(operator(add))),
+            v(hnf(tuple([
+                v(hnf(integer(10))),
+                v(hnf(integer(20)))
+            ])))
+        ))
+    ).
 
-test_desugar_gt :-
-    % Act
-    phrase(p(X), "fail>fail", []),
-    % Assert
-    X=one(e(application(gt, tuple(e(fail), e(fail))))).
+test_app_gt :-
+    phrase(p(X), "10>20"),
+    phrase(p(X), "gt <10, 20>"),
+    X = one(
+        e(application(
+            v(hnf(operator(gt))),
+            v(hnf(tuple([
+                v(hnf(integer(10))),
+                v(hnf(integer(20)))
+            ])))
+        ))
+    ).
 
 test_desugar_variable_initialisation :-
-    % Act
+    phrase(p(X), "Epeter. peter=fail, fail")
     phrase(p(X), "peter:=fail; fail", []),
-    % Assert
     X=one(e(exists(variable("peter"), e(eqe(eq(variable("peter"), eq(e(fail))), e(fail)))))).
