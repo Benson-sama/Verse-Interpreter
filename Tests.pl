7d8a807bab6b01e033688fc297320c5e44b449c2
Benson-sama <BOGNER.Benjamin@outlook.at>
2023-05-15 09:33:16 +0200

    Updated the initial three test cases.

--- Tests.pl
+++ Tests.pl
@@ -1,23 +1,22 @@
-% This file still applies to a previous version where strings were parsed.
-% Therefore this is not working anymore.
-
 test_all :-
-    test_desugar_add.
+    test_desugar_add,
+    test_desugar_gt,
+    test_desugar_variable_initialisation.
 
 test_desugar_add :-
     % Act
-    phrase(p(X), "one{fail+fail}", []),
+    phrase(p(X), "fail+fail", []),
     % Assert
     X=one(e(application(add, tuple(e(fail), e(fail))))).
 
 test_desugar_gt :-
     % Act
-    phrase(p(X), "one{fail>fail}", []),
+    phrase(p(X), "fail>fail", []),
     % Assert
-    X="one{gt<fail, fail>}".
+    X=one(e(application(gt, tuple(e(fail), e(fail))))).
 
 test_desugar_variable_initialisation :-
     % Act
-    phrase(p(X), "one{x := fail; fail}", []),
+    phrase(p(X), "peter:=fail; fail", []),
     % Assert
-    X="one{Ex. x=fail; fail}".
+    X=one(e(exists(variable("peter"), e(eqe(eq(variable("peter"), eq(e(fail))), e(fail)))))).
