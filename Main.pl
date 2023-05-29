ba1b9739a34155fa4fb5ab454b9540bb47d0892f
Benjamin BOGNER <BOGNER.Benjamin@outlook.at>
2023-05-29 23:04:46 +0200

    Implemented core logic for interpreting.

--- Main.pl
+++ Main.pl
@@ -1,22 +1,39 @@
 :- [init, syntax, rewriting, tests].
 
+% The entry point of the application.
 main :-
-    tokenise(Ts),
-    writeln(Ts).
+    prompt_user_input('Please enter the Verse code to interpret:', Cs),
+    interpret(Cs, _).
 
-% Development predicate.
-not_implemented :-
-    writeln('Not implemented yet.'),
-    false.
+% Retrieves a list of characters from the user by displaying the given prompt.
+prompt_user_input(Prompt, Cs) :-
+    writeln(Prompt),
+    read_line_to_string(user_input, S),
+    string_chars(S, Cs).
 
+% Tokenises and rewrites the given list of characters.
 interpret(Verse, Result) :-
+    write('Tokenising... '),
     tokenise(Verse, Ts),
-    rewrite(Ts, Result).
+    writeln(Ts),
+    write('Rewriting... '),
+    apply_rewrite(Ts, E),
+    result(E, Result),
+    write('\nResult: '),
+    writeln(Result).
 
+result(e(v(hnf(integer(I)))), I).
+result(e(v(hnf(tuple(T)))), T).
+result(e(fail), fail).
+
+% Tokenises the given list of characters.
 tokenise(Cs, Ts) :- phrase(p(Ts), Cs).
 
-tokenise(Ts) :- tokenise_file('samples/main.verse', Ts).
+% Helper predicate to quickly tokenise the Main.verse file via the console.
+tokenise(Ts) :- tokenise_file('Samples/Main.verse', Ts).
 
+% The conversion using phrase was necessary as alternatives lead to unexpected failure.
+% This is because of the prolog flag double_quotes chars.
 tokenise_file(File, Ts) :-
     read_file_to_string(File, I, []),
     writeln('Tokenising:'),
@@ -25,3 +42,8 @@ tokenise_file(File, Ts) :-
     phrase(lines(Ls), Cs),
     phrase(seqq(Ls), S),
     phrase(p(Ts), S).
+
+% Development predicate.
+not_implemented :-
+    writeln('Not implemented yet.'),
+    false.
