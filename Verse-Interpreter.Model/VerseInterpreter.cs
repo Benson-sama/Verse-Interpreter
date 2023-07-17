f4fb7ca5a92738ff090b49ed654b5ebc337670d4
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-07-17 19:15:30 +0200

    Add syntax tree builder in interpreter.

--- Verse-Interpreter.Model/VerseInterpreter.cs
+++ Verse-Interpreter.Model/VerseInterpreter.cs
@@ -1,19 +1,35 @@
 ﻿using Antlr4.Runtime;
+using Microsoft.Extensions.Logging;
 
 namespace Verse_Interpreter.Model;
 
 public class VerseInterpreter
 {
-    public VerseParser.ProgramContext GenerateParseTreeFromString(string input)
+    private readonly ILogger _logger;
+    private readonly IVerseSyntaxTreeBuilder _syntaxTreeBuilder;
+
+    public VerseInterpreter(ILogger<VerseInterpreter> logger, IVerseSyntaxTreeBuilder syntaxTreeBuilder)
     {
+        _logger = logger;
+        _syntaxTreeBuilder = syntaxTreeBuilder;
+    }
+
+    public VerseProgram GenerateParseTreeFromString(string input)
+    {
+        _logger.LogInformation("Getting input stream...");
         ICharStream stream = CharStreams.fromString(input);
+        _logger.LogInformation("Starting lexer...");
         ITokenSource lexer = new VerseLexer(stream);
+        _logger.LogInformation("Getting token stream...");
         ITokenStream tokens = new CommonTokenStream(lexer);
+        _logger.LogInformation("Starting parser...");
         VerseParser parser = new(tokens)
         {
             BuildParseTree = true
         };
 
-        return parser.program();
+        _logger.LogInformation("Finished generating parse tree. Converting and desugaring...");
+        VerseParser.ProgramContext programContext = parser.program();
+        return _syntaxTreeBuilder.BuildCustomSyntaxTree(programContext);
     }
 }
