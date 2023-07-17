05906733c1aaeb7efa869e03885f7039256e8389
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-07-17 14:48:27 +0200

    Create Verse1.cs

---
+++ Verse-Interpreter.Test/Verse1.cs
@@ -0,0 +1,113 @@
+﻿namespace Verse_Interpreter.Test;
+
+[TestClass]
+internal class Verse1
+{
+    [TestMethod]
+    public void TestAssignment()
+    {
+        // Arrange.
+        string code = "x:=3; x";
+
+        // Act.
+
+        // Assert.
+        // Result: 3
+    }
+
+    [TestMethod]
+    public void TestAddition()
+    {
+        // Arrange.
+        string code = "x:=3; x+4";
+
+        // Act.
+
+        // Assert.
+        // Result: 7
+    }
+
+    [TestMethod]
+    public void TestSubtraction()
+    {
+        // Arrange.
+        string code = "x:=3; x-2";
+
+        // Act.
+
+        // Assert.
+        // Result: 1
+    }
+
+    [TestMethod]
+    public void TestMultiplication()
+    {
+        // Arrange.
+        string code = "x:=3; x*5";
+
+        // Act.
+
+        // Assert.
+        // Result: 15
+    }
+
+    [TestMethod]
+    public void TestDivision()
+    {
+        // Arrange.
+        string code = "x:=6; x/2";
+
+        // Act.
+
+        // Assert.
+        // Result: 3
+    }
+
+    [TestMethod]
+    public void TestFunkyOrder()
+    {
+        // Arrange.
+        string code = "y:=x+1; x:=3; x*y";
+
+        // Act.
+
+        // Assert.
+        // Result: 15
+    }
+
+    [TestMethod]
+    public void TestFunction()
+    {
+        // Arrange.
+        string code = "f(x):=x+1; f(3)";
+
+        // Act.
+
+        // Assert.
+        // Result: 4
+    }
+
+    [TestMethod]
+    public void TestLambda()
+    {
+        // Arrange.
+        string code = "f:=(x=>x+1); f(3)";
+
+        // Act.
+
+        // Assert.
+        // Result: 4
+    }
+
+    [TestMethod]
+    public void TestFactorialFunction()
+    {
+        // Arrange.
+        string code = "fac(x):= if(x=0): 1 else: x * fac(x-1)";
+
+        // Act.
+
+        // Assert.
+        // Result: 4
+    }
+}
