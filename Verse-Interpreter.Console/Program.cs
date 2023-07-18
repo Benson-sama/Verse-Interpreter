29b5456f31e041434ef28d71d73ec1affb8cafc4
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-07-18 16:18:18 +0200

    Update using directives.

--- Verse-Interpreter.Console/Program.cs
+++ Verse-Interpreter.Console/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Verse_Interpreter.Model;
+using Verse_Interpreter.Model.SyntaxTree;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Values;
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
