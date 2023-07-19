516613444429f1df4697d946219db93552a071ba
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-07-19 17:10:00 +0200

    Implement most of the syntax building.

--- Verse-Interpreter.Model/VerseSyntaxTreeBuilder.cs
+++ Verse-Interpreter.Model/VerseSyntaxTreeBuilder.cs
@@ -1,17 +1,295 @@
-﻿namespace Verse_Interpreter.Model;
+﻿using Verse_Interpreter.Model.SyntaxTree;
+using Verse_Interpreter.Model.SyntaxTree.Expressions;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Equations;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Values;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms.Operators;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Wrappers;
+using Tuple = Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms.Tuple;
+
+namespace Verse_Interpreter.Model;
 
 public class VerseSyntaxTreeBuilder : IVerseSyntaxTreeBuilder
 {
-    private readonly IVerseVisitor<Node> _verseVisitor;
-
-    public VerseSyntaxTreeBuilder(IVerseVisitor<Node> verseVisitor)
-        => _verseVisitor = verseVisitor;
-
-    public VerseProgram BuildCustomSyntaxTree(VerseParser.ProgramContext context)
+    public VerseProgram BuildCustomSyntaxTreeWrappedInOne(VerseParser.ProgramContext context)
     {
-        if (context.Accept(_verseVisitor) is not VerseProgram program)
-            throw new Exception("Unable to parse the verse program.");
+        return new VerseProgram()
+        {
+            Wrapper = new One()
+            {
+                E = GetExpression(context.e())
+            }
+        };
+    }
 
-        return program;
+    public VerseProgram BuildCustomSyntaxTreeWrappedInAll(VerseParser.ProgramContext context)
+    {
+        return new VerseProgram()
+        {
+            Wrapper = new All()
+            {
+                E = GetExpression(context.e())
+            }
+        };
+    }
+
+    private Expression GetExpression(VerseParser.EContext context) => context switch
+    {
+        VerseParser.ParenthesisExpContext c => GetExpression(c.e()),
+        VerseParser.MultExpContext c => GetConcreteExpression(c),
+        VerseParser.DivExpContext c => GetConcreteExpression(c),
+        VerseParser.PlusExpContext c => GetConcreteExpression(c),
+        VerseParser.MinusExpContext c => GetConcreteExpression(c),
+        VerseParser.GreaterThanExpContext c => GetConcreteExpression(c),
+        VerseParser.LessThanExpContext c => GetConcreteExpression(c),
+        VerseParser.AssignmentExpContext c => GetConcreteExpression(c),
+        VerseParser.ValueExpContext c => GetValue(c.v()),
+        VerseParser.FailExpContext => new Fail(),
+        VerseParser.RangeChoiceExpContext c => GetConcreteExpression(c),
+        VerseParser.ChoiceExpContext c => GetConcreteExpression(c),
+        VerseParser.ValueApplicationExpContext c => GetConcreteExpression(c),
+        VerseParser.ExpApplicationExpContext c => GetConcreteExpression(c),
+        VerseParser.ExpEquationExpContext c => GetConcreteExpression(c),
+        VerseParser.IfElseExpContext c => GetConcreteExpression(c),
+        VerseParser.ForExpContext c => GetConcreteExpression(c),
+        VerseParser.EqeExpContext c => GetConcreteExpression(c),
+        { } => throw new Exception($"Unable to match context type: {context.GetType()}"),
+        _ => throw new ArgumentNullException(nameof(context), "Cannot be null or empty.")
+    };
+
+    private Expression GetConcreteExpression(VerseParser.MultExpContext context)
+    {
+        // TODO: Ensure freshness!
+        Expression e1 = GetExpression(context.e(0));
+        Expression e2 = GetExpression(context.e(1));
+        Operator mult = new Mult();
+
+        return BuildArithmeticExpression(e1, mult, e2);
+    }
+
+    private Expression GetConcreteExpression(VerseParser.DivExpContext context)
+    {
+        // TODO: Ensure freshness!
+        Expression e1 = GetExpression(context.e(0));
+        Expression e2 = GetExpression(context.e(1));
+        Operator div = new Div();
+
+        return BuildArithmeticExpression(e1, div, e2);
+    }
+
+    private Expression GetConcreteExpression(VerseParser.PlusExpContext context)
+    {
+        // TODO: Ensure freshness!
+        Expression e1 = GetExpression(context.e(0));
+        Expression e2 = GetExpression(context.e(1));
+        Operator add = new Add();
+
+        return BuildArithmeticExpression(e1, add, e2);
+    }
+
+    private Expression GetConcreteExpression(VerseParser.MinusExpContext context)
+    {
+        // TODO: Ensure freshness!
+        Expression e1 = GetExpression(context.e(0));
+        Expression e2 = GetExpression(context.e(1));
+        Operator sub = new Sub();
+
+        return BuildArithmeticExpression(e1, sub, e2);
+    }
+
+    private Expression GetConcreteExpression(VerseParser.GreaterThanExpContext context)
+    {
+        // TODO: Ensure freshness!
+        Expression e1 = GetExpression(context.e(0));
+        Expression e2 = GetExpression(context.e(1));
+        Operator greaterThan = new Gt();
+
+        return BuildArithmeticExpression(e1, greaterThan, e2);
+    }
+
+    private Expression GetConcreteExpression(VerseParser.LessThanExpContext context)
+    {
+        // TODO: Ensure freshness!
+        Expression e1 = GetExpression(context.e(0));
+        Expression e2 = GetExpression(context.e(1));
+        Operator lessThan = new Lt();
+
+        return BuildArithmeticExpression(e1, lessThan, e2);
+    }
+
+    private Expression GetConcreteExpression(VerseParser.AssignmentExpContext context)
+    {
+        Expression e1 = GetExpression(context.e(0));
+        Variable v = new(context.VARIABLE().GetText());
+        Equation eq = new()
+        {
+            V = v,
+            E = e1
+        };
+
+        if (context.ChildCount is 3)
+            return eq;
+
+        Expression e2 = GetExpression(context.e(1));
+        return new Eqe()
+        {
+            Eq = eq,
+            E = e2
+        };
+    }
+
+    private Expression GetConcreteExpression(VerseParser.RangeChoiceExpContext context)
+    {
+        int i1 = int.Parse(context.INTEGER(0).GetText());
+        int i2 = int.Parse(context.INTEGER(1).GetText());
+
+        if (i1 > i2)
+            throw new Exception("Invalid range expression, the first value cannot be greater than the second.");
+
+        return BuildIntegerChoiceRecursively(i1, i2);
+    }
+
+    private Expression GetConcreteExpression(VerseParser.ChoiceExpContext context)
+    {
+        Expression e1 = GetExpression(context.e(0));
+        Expression e2 = GetExpression(context.e(1));
+
+        return new Choice()
+        {
+            E1 = e1,
+            E2 = e2
+        };
+    }
+
+    private Expression GetConcreteExpression(VerseParser.ValueApplicationExpContext context)
+    {
+        Value v1 = GetValue(context.v(0));
+        Value v2 = GetValue(context.v(1));
+
+        return new Application()
+        {
+            V1 = v1,
+            V2 = v2
+        };
+    }
+
+    private Expression GetConcreteExpression(VerseParser.ExpApplicationExpContext context)
+    {
+        // TODO: Implement.
+        return new Expression();
+    }
+
+    private Expression GetConcreteExpression(VerseParser.ExpEquationExpContext context)
+    {
+        // TODO: Implement.
+        return new Expression();
+    }
+
+    private Expression GetConcreteExpression(VerseParser.IfElseExpContext context)
+    {
+        // TODO: Implement.
+        return new Expression();
+    }
+
+    private Expression GetConcreteExpression(VerseParser.ForExpContext context)
+    {
+        // TODO: Implement.
+        return new Expression();
+    }
+
+    private Expression GetConcreteExpression(VerseParser.EqeExpContext context)
+    {
+        // TODO: Implement.
+        return new Expression();
+    }
+
+    private Value GetValue(VerseParser.VContext context)
+    {
+        return context switch
+        {
+            VerseParser.VariableValueContext c => new Variable(c.VARIABLE().GetText()),
+            VerseParser.HnfValueContext c => GetHeadNormalForm(c.hnf()),
+            { } => throw new Exception("Unable to match value context."),
+            _ => throw new ArgumentNullException(nameof(context), "Cannot be null.")
+        };
+    }
+
+    private Tuple GetTuple(VerseParser.TupleContext tupleContext)
+    {
+        // TODO: Implement Tuple parsing.
+        return new Tuple() { Values = Array.Empty<Tuple>() };
+    }
+
+    private HeadNormalForm GetHeadNormalForm(VerseParser.HnfContext context)
+    {
+        return context switch
+        {
+            VerseParser.IntegerHnfContext c => new Integer(int.Parse(c.INTEGER().GetText())),
+            VerseParser.TupleHnfContext c => GetTuple(c.tuple()),
+            VerseParser.LambdaHnfContext c => GetLambda(c.lambda()),
+            { } => throw new Exception("Unable to match value context."),
+            _ => throw new ArgumentNullException(nameof(context), "Cannot be null.")
+        };
+    }
+
+    private Lambda GetLambda(VerseParser.LambdaContext lambdaContext)
+    {
+        // TODO: Implement lambda parsing.
+        return new Lambda();
+    }
+
+    private static Expression BuildIntegerChoiceRecursively(int min, int max)
+    {
+        if (min == max)
+            return new Integer(max);
+
+        return new Choice()
+        {
+            E1 = new Integer(min),
+            E2 = BuildIntegerChoiceRecursively(min + 1, max)
+        };
+    }
+
+    private static Exists BuildArithmeticExpression(Expression e1, Operator op, Expression e2)
+    {
+        Variable v1 = new(Guid.NewGuid().ToString());
+        Variable v2 = new(Guid.NewGuid().ToString());
+
+        return new Exists()
+        {
+            V = v1,
+            E = new Eqe()
+            {
+                Eq = new Equation()
+                {
+                    V = v1,
+                    E = e1
+                },
+                E = new Exists()
+                {
+                    V = v2,
+                    E = new Eqe()
+                    {
+                        Eq = new Equation()
+                        {
+                            V = v2,
+                            E = e2
+                        },
+                        E = new Application()
+                        {
+                            V1 = op,
+                            V2 = new Tuple()
+                            {
+                                Values = new Value[]
+                                {
+                                    v1,
+                                    v2
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
     }
 }
