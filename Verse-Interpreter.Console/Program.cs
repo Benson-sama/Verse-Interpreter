6c614786b618848fccce3d95e1fe4c3a4108470e
Benson-sama <122638@fhwn.ac.at>
2023-08-03 17:13:46 +0200

    Use new interpret method.

--- Verse-Interpreter.Console/Program.cs
+++ Verse-Interpreter.Console/Program.cs
@@ -1,13 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Verse_Interpreter.Model;
-using Verse_Interpreter.Model.SyntaxTree;
-using Verse_Interpreter.Model.SyntaxTree.Expressions;
-using Verse_Interpreter.Model.SyntaxTree.Expressions.Equations;
-using Verse_Interpreter.Model.SyntaxTree.Expressions.Values;
-using Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms;
-using Verse_Interpreter.Model.SyntaxTree.Expressions.Wrappers;
-using Tuple = Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms.Tuple;
 
 Console.WriteLine("-- Verse-Interpreter Console --");
 
@@ -21,58 +14,8 @@ var host = Host.CreateDefaultBuilder(args)
     .Build();
 
 VerseInterpreter verseInterpreter = host.Services.GetRequiredService<VerseInterpreter>();
-string input = File.OpenText(@"C:\Users\User\Documents\GitHub\Verse-Interpreter\Input.verse")
-                   .ReadToEnd();
-VerseProgram verseProgram = verseInterpreter.GenerateParseTreeFromString(input);
-
-Variable x = new(nameof(x));
-Variable y = new(nameof(y));
-
-VerseProgram customVerseProgram = new()
-{
-    Wrapper = new One()
-    {
-        E = new Exists()
-        {
-            V = x,
-            E = new Exists()
-            {
-                V = y,
-                E = new Eqe()
-                {
-                    Eq = new Equation()
-                    {
-                        V = x,
-                        E = new Choice()
-                        {
-                            E1 = new Integer(7),
-                            E2 = new Integer(22)
-                        }
-                    },
-                    E = new Eqe()
-                    {
-                        Eq = new Equation()
-                        {
-                            V = y,
-                            E = new Choice()
-                            {
-                                E1 = new Integer(31),
-                                E2 = new Integer(5)
-                            }
-                        },
-                        E = new Tuple()
-                        {
-                            Values = new Value[]
-                            {
-                                x,
-                                y
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-};
+string verseCode = File.OpenText(@"C:\Users\User\source\repos\Benson-sama\Verse-Interpreter\Input.verse")
+                       .ReadToEnd();
+verseInterpreter.Interpret(verseCode);
 
 Console.WriteLine("Done.");
