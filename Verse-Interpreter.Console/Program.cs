9fdec3db9f5664f9e211d2185b601bb4384a01bb
Benson-sama <122638@fhwn.ac.at>
2023-08-04 12:29:24 +0200

    Add several commands for code input.

--- Verse-Interpreter.Console/Program.cs
+++ Verse-Interpreter.Console/Program.cs
@@ -14,8 +14,75 @@ var host = Host.CreateDefaultBuilder(args)
     .Build();
 
 VerseInterpreter verseInterpreter = host.Services.GetRequiredService<VerseInterpreter>();
-string verseCode = File.OpenText(@"C:\Users\Benjamin\source\repos\Benson-sama\Verse-Interpreter\Input.verse")
-                       .ReadToEnd();
-verseInterpreter.Interpret("xs:=(1,2); head(xs)");
-
+GetRequestedCommand(args)();
 Console.WriteLine("Done.");
+
+Action GetRequestedCommand(string[] args)
+{
+    return args.FirstOrDefault() switch
+    {
+        "-code" => () => ExecuteCodeCommand(args.ElementAtOrDefault(1)),
+        "-interactive" => ExecuteInteractiveCommand,
+        "-file" => ExecuteFileCommand,
+        { } => ExecuteUnknownCommand,
+        _ => ExecuteMissingCommand
+    };
+}
+
+void ExecuteCodeCommand(string? verseCode)
+{
+    if (verseCode is null)
+    {
+        Console.WriteLine("Supplied code is invalid.");
+        return;
+    }
+
+    verseInterpreter.Interpret(verseCode);
+}
+
+void ExecuteInteractiveCommand()
+{
+    Console.WriteLine("Please enter your Verse code:\n");
+    string? verseCode = Console.ReadLine();
+
+    if (verseCode is null)
+        Console.WriteLine("No verse code entered.");
+    else
+        verseInterpreter.Interpret(verseCode);
+}
+
+void ExecuteFileCommand()
+{
+    string filePath = GetFilePathFromUser();
+    using StreamReader sr = File.OpenText(filePath);
+    string verseCode = sr.ReadToEnd();
+    verseInterpreter.Interpret(verseCode);
+}
+
+void ExecuteUnknownCommand()
+    => Console.WriteLine("Unknown command specified.");
+
+void ExecuteMissingCommand()
+{
+    Console.WriteLine("""
+        No command specified. Possible commands are:
+
+          -code {code}
+          -interactive
+          -file {filePath}
+        
+        """);
+}
+
+static string GetFilePathFromUser()
+{
+    string? path;
+
+    do
+    {
+        Console.WriteLine("Please enter the path of the file containing the verse code.");
+        path = Console.ReadLine();
+    } while (!File.Exists(path));
+
+    return path;
+}
