05141e566f284647314326e2fd64af2c54f6f92c
Benson-sama <122638@fhwn.ac.at>
2023-08-05 20:04:15 +0200

    Add prototype for rewriting.

--- Verse-Interpreter.Model/VerseInterpreter.cs
+++ Verse-Interpreter.Model/VerseInterpreter.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Logging;
 using Verse_Interpreter.Model.SyntaxTree;
 using Verse_Interpreter.Model.SyntaxTree.Expressions;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Equations;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Wrappers;
 
 namespace Verse_Interpreter.Model;
 
@@ -9,11 +12,13 @@ public class VerseInterpreter
 {
     private readonly ILogger _logger;
     private readonly IVerseSyntaxTreeBuilder _syntaxTreeBuilder;
+    private readonly IRewriter _rewriter;
 
-    public VerseInterpreter(ILogger<VerseInterpreter> logger, IVerseSyntaxTreeBuilder syntaxTreeBuilder)
+    public VerseInterpreter(ILogger<VerseInterpreter> logger, IVerseSyntaxTreeBuilder syntaxTreeBuilder, IRewriter rewriter)
     {
         _logger = logger;
         _syntaxTreeBuilder = syntaxTreeBuilder;
+        _rewriter = rewriter;
     }
 
     public Expression Interpret(string verseCode)
@@ -22,8 +27,97 @@ public class VerseInterpreter
         return Rewrite(verseProgram);
     }
 
+    private void Rewrite(Expression expression)
+    {
+        switch (expression)
+        {
+            case Eqe eqe:
+                Rewrite(eqe);
+                break;
+            case Equation eq:
+                Rewrite(eq);
+                break;
+            case Lambda lambda:
+                Rewrite(lambda);
+                break;
+            case Application application:
+                Rewrite(application);
+                break;
+            case Choice choice:
+                Rewrite(choice);
+                break;
+            case Exists exists:
+                Rewrite(exists);
+                break;
+            case Wrapper wrapper:
+                Rewrite(wrapper);
+                break;
+        }
+    }
+
     private Expression Rewrite(VerseProgram verseProgram)
-        => throw new NotImplementedException();
+    {
+        do
+        {
+            verseProgram.Wrapper.E = _rewriter.TryRewrite(verseProgram.Wrapper.E);
+
+            if (!_rewriter.RuleApplied)
+                Rewrite(verseProgram.Wrapper.E);
+        }
+        while (!_rewriter.RuleApplied);
+
+        return _rewriter.TryRewrite(verseProgram.Wrapper);
+    }
+
+    private void Rewrite(Eqe eqe)
+    {
+        eqe.Eq = _rewriter.TryRewrite(eqe.Eq);
+
+        if (_rewriter.RuleApplied)
+            return;
+
+        Rewrite(eqe.Eq);
+
+        if (_rewriter.RuleApplied)
+            return;
+
+        eqe.E = _rewriter.TryRewrite(eqe.E);
+
+        if (_rewriter.RuleApplied)
+            return;
+
+        Rewrite(eqe.E);
+    }
+
+    private void Rewrite(Equation eq)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Rewrite(Lambda lambda)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Rewrite(Application application)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Rewrite(Choice choice)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Rewrite(Exists exists)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Rewrite(Wrapper wrapper)
+    {
+        throw new NotImplementedException();
+    }
 
     public VerseProgram GenerateParseTreeFromString(string input)
     {
