b26fdcea3218ee018bab28be7f364f7489954942
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-08-06 23:18:51 +0200

    Add new logger.

---
+++ Verse-Interpreter.Console/ConsoleVerseLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse_Interpreter.Model;
+
+namespace Verse_Interpreter.Console;
+
+public class ConsoleVerseLogger : IVerseLogger
+{
+    public void Log(string message)
+        => System.Console.WriteLine(message);
+
+    public void LogRuleApplied(string message)
+        => System.Console.Write($"[~{message}] ");
+}
---
+++ Verse-Interpreter.Model/IVerseLogger.cs
@@ -0,0 +1,8 @@
+﻿namespace Verse_Interpreter.Model;
+
+public interface IVerseLogger
+{
+    void Log(string message);
+
+    void LogRuleApplied(string message);
+}
