7b4a2533be1f118638876a407f41e7d8374fb356
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-08-06 22:02:40 +0200

    Add U-LIT, VAL-ELIM, EXI-SWAP, ONE-FAIL, ONE-VALUE.

--- Verse-Interpreter.Model/Rewriter.cs
+++ Verse-Interpreter.Model/Rewriter.cs
@@ -1,7 +1,9 @@
 ﻿using Verse_Interpreter.Model.SyntaxTree.Expressions;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Equations;
 using Verse_Interpreter.Model.SyntaxTree.Expressions.Values;
 using Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms;
 using Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms.Operators;
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Wrappers;
 using Tuple = Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms.Tuple;
 
 namespace Verse_Interpreter.Model;
@@ -14,8 +16,18 @@ public class Rewriter : IRewriter
     {
         _rewriteRules = new Func<Expression, Expression>[]
         {
+            // Application.
             TryRewriteWithAppAdd,
-            TryRewriteWithAppGtAndAppGtFail
+            TryRewriteWithAppGtAndAppGtFail,
+            // Unification.
+            TryRewriteWithULit,
+            // Elimination.
+            TryRewriteWithValElim,
+            // Normalisation.
+            TryRewriteWithExiSwap,
+            // Choice.
+            TryRewriteWithOneFail,
+            TryRewriteWithOneValue
         };
     }
 
@@ -45,6 +57,7 @@ public class Rewriter : IRewriter
             {
                 Expression rewrittenExpression = new Integer(i1.Value + i2.Value);
                 // TODO: Output ~APP-ADD
+                Console.Write("[~APP-ADD] ");
                 RuleApplied = true;
                 return rewrittenExpression;
             }
@@ -64,6 +77,7 @@ public class Rewriter : IRewriter
                     Expression rewrittenExpression = i1;
                     RuleApplied = true;
                     // TODO: Output ~APP-GT
+                    Console.Write("[~APP-GT] ");
                     return rewrittenExpression;
                 }
                 else
@@ -71,6 +85,7 @@ public class Rewriter : IRewriter
                     Expression rewrittenExpression = new Fail();
                     RuleApplied = true;
                     // TODO: Output ~APP-GT-FAIL
+                    Console.Write("[~APP-GT-FAIL] ");
                     return rewrittenExpression;
                 }
             }
@@ -79,15 +94,83 @@ public class Rewriter : IRewriter
         return expression;
     }
 
-    [Obsolete]
-    private RewriteResult TryRewriteAppAddWithSwitch(Expression expression)
+    private Expression TryRewriteWithULit(Expression expression)
     {
-        return expression switch
+        if (expression is Eqe { Eq: Equation { V: Integer k1, E: Integer k2 }, E: Expression e })
         {
-            Application { V1: Add, V2: Tuple { Values: IEnumerable<Value> values } }
-                when values.Count() is 2 && values.ElementAt(0) is Integer i1 && values.ElementAt(1) is Integer i2 =>
-                    new RewriteResult(RuleApplied: true, RewrittenExpression: new Integer(i1.Value + i2.Value), AppliedRuleName: "APP-ADD"),
-            _ => new RewriteResult(RuleApplied: false, expression)
-        };
+            if (k1.Value == k2.Value)
+            {
+                RuleApplied = true;
+                // TODO: Output ~U-LIT
+                Console.Write("[~U-LIT] ");
+                return e;
+            }
+        }
+
+        return expression;
     }
+
+    private Expression TryRewriteWithValElim(Expression expression)
+    {
+        if (expression is Eqe { Eq: Value v, E: Expression e })
+        {
+            RuleApplied = true;
+            // TODO: Output ~VAL-ELIM
+            Console.Write("[~VAL-ELIM] ");
+            return e;
+        }
+
+        return expression;
+    }
+
+    private Expression TryRewriteWithExiSwap(Expression expression)
+    {
+        if (expression is Exists { V: Variable x, E: Exists { V: Variable y, E: Expression e } existsY } existsX)
+        {
+            RuleApplied = true;
+            // TODO: Output ~EXI-SWAP
+            Console.Write("[~EXI-SWAP] ");
+            return existsY.E = existsX.E = e;
+        }
+
+        return expression;
+    }
+
+    private Expression TryRewriteWithOneFail(Expression expression)
+    {
+        if (expression is One { E: Fail fail })
+        {
+            RuleApplied = true;
+            // TODO: Output ~ONE-FAIL
+            Console.Write("[~ONE-FAIL] ");
+            return fail;
+        }
+
+        return expression;
+    }
+
+    private Expression TryRewriteWithOneValue(Expression expression)
+    {
+        if (expression is One { E: Value value })
+        {
+            RuleApplied = true;
+            // TODO: Output ~ONE-VALUE
+            Console.Write("[~ONE-VALUE] ");
+            return value;
+        }
+
+        return expression;
+    }
+
+    //[Obsolete]
+    //private RewriteResult TryRewriteAppAddWithSwitch(Expression expression)
+    //{
+    //    return expression switch
+    //    {
+    //        Application { V1: Add, V2: Tuple { Values: IEnumerable<Value> values } }
+    //            when values.Count() is 2 && values.ElementAt(0) is Integer i1 && values.ElementAt(1) is Integer i2 =>
+    //                new RewriteResult(RuleApplied: true, RewrittenExpression: new Integer(i1.Value + i2.Value), AppliedRuleName: "APP-ADD"),
+    //        _ => new RewriteResult(RuleApplied: false, expression)
+    //    };
+    //}
 }
