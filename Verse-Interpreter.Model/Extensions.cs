4d4db2e3c03ecdc9f5cbbcf7822d9f0c6d9f4e1d
Benson-sama <122638@fhwn.ac.at>
2023-08-14 18:17:03 +0200

    Add alpha conversion.

--- Verse-Interpreter.Model/Extensions.cs
+++ Verse-Interpreter.Model/Extensions.cs
@@ -9,7 +9,12 @@ namespace Verse_Interpreter.Model;
 public static class Extensions
 {
     public static IEnumerable<Variable> FreeVariables(this Expression expression)
-        => expression.FreeVariables(new VariableBuffer());
+    {
+        VariableBuffer variableBuffer = new();
+        expression.FreeVariables(variableBuffer);
+
+        return variableBuffer.FreeVariables;
+    }
 
     public static IEnumerable<Variable> FreeVariables(this Expression expression, VariableBuffer variableBuffer)
     {
@@ -30,37 +35,33 @@ public static class Extensions
 
     private static IEnumerable<Variable> FreeVariables(this Eqe eqe, VariableBuffer variableBuffer)
     {
-        eqe.Eq.FreeVariables(variableBuffer);
+        _ = eqe.Eq.FreeVariables(variableBuffer);
         return eqe.E.FreeVariables(variableBuffer);
     }
 
     private static IEnumerable<Variable> FreeVariables(this Equation eq, VariableBuffer variableBuffer)
     {
-        eq.V.FreeVariables(variableBuffer);
+        _ = eq.V.FreeVariables(variableBuffer);
         return eq.E.FreeVariables(variableBuffer);
     }
 
     private static IEnumerable<Variable> FreeVariables(this Lambda lambda, VariableBuffer variableBuffer)
     {
-        if (lambda.Parameter is not null)
-            variableBuffer.BoundVariables = variableBuffer.BoundVariables.Append(lambda.Parameter);
-
+        variableBuffer.BoundVariables = variableBuffer.BoundVariables.Append(lambda.Parameter);
         return lambda.E.FreeVariables(variableBuffer);
     }
 
     private static IEnumerable<Variable> FreeVariables(this VerseTuple tuple, VariableBuffer variableBuffer)
     {
         foreach (Value value in tuple)
-        {
-            value.FreeVariables(variableBuffer);
-        }
+            _ = value.FreeVariables(variableBuffer);
 
         return variableBuffer.FreeVariables;
     }
 
     private static IEnumerable<Variable> FreeVariables(this Variable variable, VariableBuffer variableBuffer)
     {
-        if (!variableBuffer.BoundVariables.Contains(variable))
+        if (!variableBuffer.BoundVariables.Contains(variable) && !variableBuffer.FreeVariables.Contains(variable))
             variableBuffer.FreeVariables = variableBuffer.FreeVariables.Append(variable);
 
         return variableBuffer.FreeVariables;
@@ -68,13 +69,13 @@ public static class Extensions
 
     private static IEnumerable<Variable> FreeVariables(this Application application, VariableBuffer variableBuffer)
     {
-        application.V1.FreeVariables(variableBuffer);
+        _ = application.V1.FreeVariables(variableBuffer);
         return application.V2.FreeVariables(variableBuffer);
     }
 
     private static IEnumerable<Variable> FreeVariables(this Choice choice, VariableBuffer variableBuffer)
     {
-        choice.E1.FreeVariables(variableBuffer);
+        _ = choice.E1.FreeVariables(variableBuffer);
         return choice.E2.FreeVariables(variableBuffer);
     }
 
@@ -86,4 +87,142 @@ public static class Extensions
 
     private static IEnumerable<Variable> FreeVariables(this Wrapper wrapper, VariableBuffer variableBuffer)
         => wrapper.E.FreeVariables(variableBuffer);
+
+    public static void ApplyAlphaConversion(this Expression expression, Variable previousVariable, Variable newVariable)
+    {
+        switch (expression)
+        {
+            case Eqe eqe:
+                eqe.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+            case Equation equation:
+                equation.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+            case Value value:
+                value.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+            case Application application:
+                application.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+            case Choice choice:
+                choice.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+            case Exists exists:
+                exists.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+            case Wrapper wrapper:
+                wrapper.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+        }
+    }
+
+    private static void ApplyAlphaConversion(this Eqe eqe, Variable previousVariable, Variable newVariable)
+    {
+        if (eqe.Eq is Variable v1 && v1.Equals(previousVariable))
+            eqe.Eq = newVariable;
+        else
+            eqe.Eq.ApplyAlphaConversion(previousVariable, newVariable);
+
+        if (eqe.E is Variable v2 && v2.Equals(previousVariable))
+            eqe.E = newVariable;
+        else
+            eqe.E.ApplyAlphaConversion(previousVariable, newVariable);
+    }
+
+    private static void ApplyAlphaConversion(this Equation equation, Variable previousVariable, Variable newVariable)
+    {
+        if (equation.V is Variable v1 && v1.Equals(previousVariable))
+            equation.V = newVariable;
+        else
+            equation.V.ApplyAlphaConversion(previousVariable, newVariable);
+
+        if (equation.E is Variable v2 && v2.Equals(previousVariable))
+            equation.E = newVariable;
+        else
+            equation.E.ApplyAlphaConversion(previousVariable, newVariable);
+    }
+
+    private static void ApplyAlphaConversion(this Value value, Variable previousVariable, Variable newVariable)
+    {
+        switch (value)
+        {
+            case Lambda lambda:
+                lambda.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+            case VerseTuple tuple:
+                tuple.ApplyAlphaConversion(previousVariable, newVariable);
+                break;
+        }
+    }
+
+    private static void ApplyAlphaConversion(this Lambda lambda, Variable previousVariable, Variable newVariable)
+    {
+        if (lambda.Parameter is Variable v1 && v1.Equals(previousVariable))
+            return;
+
+        if (lambda.E is Variable v2 && v2.Equals(previousVariable))
+            lambda.E = newVariable;
+        else
+            lambda.E.ApplyAlphaConversion(previousVariable, newVariable);
+    }
+
+    private static void ApplyAlphaConversion(this VerseTuple tuple, Variable previousVariable, Variable newVariable)
+    {
+        Value[] values = tuple.ToArray();
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] is Variable v && v.Equals(previousVariable))
+                values[i] = newVariable;
+            else
+                values[i].ApplyAlphaConversion(previousVariable, newVariable);
+        }
+
+        tuple.Values = values;
+    }
+
+    private static void ApplyAlphaConversion(this Application application, Variable previousVariable, Variable newVariable)
+    {
+        if (application.V1 is Variable v1 && v1.Equals(previousVariable))
+            application.V1 = newVariable;
+        else
+            application.V1.ApplyAlphaConversion(previousVariable, newVariable);
+
+        if (application.V2 is Variable v2 && v2.Equals(previousVariable))
+            application.V2 = newVariable;
+        else
+            application.V2.ApplyAlphaConversion(previousVariable, newVariable);
+    }
+
+    private static void ApplyAlphaConversion(this Choice choice, Variable previousVariable, Variable newVariable)
+    {
+        if (choice.E1 is Variable v1 && v1.Equals(previousVariable))
+            choice.E1 = newVariable;
+        else
+            choice.E1.ApplyAlphaConversion(previousVariable, newVariable);
+
+        if (choice.E2 is Variable v2 && v2.Equals(previousVariable))
+            choice.E2 = newVariable;
+        else
+            choice.E2.ApplyAlphaConversion(previousVariable, newVariable);
+    }
+
+    private static void ApplyAlphaConversion(this Exists exists, Variable previousVariable, Variable newVariable)
+    {
+        if (exists.V.Equals(previousVariable))
+            return;
+
+        if (exists.E is Variable v && v.Equals(previousVariable))
+            exists.E = newVariable;
+        else
+            exists.E.ApplyAlphaConversion(previousVariable, newVariable);
+    }
+
+    private static void ApplyAlphaConversion(this Wrapper wrapper, Variable previousVariable, Variable newVariable)
+    {
+        if (wrapper.E is Variable v && v.Equals(previousVariable))
+            wrapper.E = newVariable;
+        else
+            wrapper.E.ApplyAlphaConversion(previousVariable, newVariable);
+    }
 }
