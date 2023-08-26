b914b05b60fb63f720186c94bc485488c25a828d
Benson-sama <122638@fhwn.ac.at>
2023-08-26 14:55:29 +0200

    Add unit test for flatMap.

--- Verse-Interpreter.Test/Verse3.cs
+++ Verse-Interpreter.Test/Verse3.cs
@@ -21,12 +21,24 @@ public class Verse3
     {
         // Arrange.
         string verseCode = "x:any; y:=x+9; x=3; x+y";
-
         // Act.
         Expression result = _verseInterpreter!.Interpret(verseCode, (e) => new One { E = e });
 
         // Assert.
         Assert.IsTrue(result is Integer { Value: 15 });
+    }
+
+    [TestMethod]
+    public void TestFlatMap()
+    {
+        // Arrange.
+        string verseCode = "flatMap:=([f,xs] => for{i:any; t:=xs(i); f[t]}); numbers:=[1,2,3,4,5]; add:=([a] => (a+1)); flatMap[add,numbers]";
+        // Act.
+        Expression result = _verseInterpreter!.Interpret(verseCode, (e) => new One { E = e });
+        ICollection tuple = (result as VerseTuple)!.ToArray();
+
+        // Assert.
+        CollectionAssert.AreEqual(tuple, new Value[] { new Integer(2), new Integer(3), new Integer(4), new Integer(5), new Integer(6) });
     }
 
     [TestMethod]
