150d62cc6198b5737aeb7b9c366c1e3f6120490c
Benson-sama <122638@fhwn.ac.at>
2023-08-29 12:53:05 +0200

    Document VerseInterpreter.

--- Verse-Interpreter.Model/VerseInterpreter.cs
+++ Verse-Interpreter.Model/VerseInterpreter.cs
@@ -1,4 +1,12 @@
-﻿using System.Diagnostics;
+﻿//-------------------------------------------------------------------
+// <copyright file="VerseInterpreter.cs" company="FH Wiener Neustadt">
+//     Copyright (c) FH Wiener Neustadt. All rights reserved.
+// </copyright>
+// <author>Benjamin Bogner</author>
+// <summary>Contains the VerseInterpreter class.</summary>
+//-------------------------------------------------------------------
+
+using System.Diagnostics;
 using Antlr4.Runtime;
 using Verse_Interpreter.Model.SyntaxTree;
 using Verse_Interpreter.Model.SyntaxTree.Expressions;
@@ -6,15 +14,49 @@ using Verse_Interpreter.Model.SyntaxTree.Expressions.Wrappers;
 
 namespace Verse_Interpreter.Model;
 
+/// <summary>
+/// Class <see cref="VerseInterpreter"/> serves as an interpreter for the
+/// functional logic programming language Verse proposed by Epic Games.
+/// </summary>
 public class VerseInterpreter
 {
+    /// <summary>
+    /// Field <c>_renderer</c> represents the rendering component used to visualise output.
+    /// </summary>
     private readonly IRenderer _renderer;
+
+    /// <summary>
+    /// Field <c>_syntaxTreeBuilder</c> represents the syntax tree building component.
+    /// It handles conversion and desugaring from ANTLR4 parse trees to a custom datatype tree.
+    /// </summary>
     private readonly IVerseSyntaxTreeBuilder _syntaxTreeBuilder;
+
+    /// <summary>
+    /// Field <c>_rewriter</c> represents the rewriter component used to handle the "execution" of the Verse program.
+    /// </summary>
     private readonly IRewriter _rewriter;
 
+    /// <summary>
+    /// This constructor initialises the new <see cref="VerseInterpreter"/> with the given components.
+    /// </summary>
+    /// <param name="renderer"><c>renderer</c> is the new VerseInterpreter's rendering component.</param>
+    /// <param name="syntaxTreeBuilder"><c>syntaxTreeBuilder</c> is the new VerseInterpreter's syntax tree building component.</param>
+    /// <param name="rewriter"><c>rewriter</c> is the new VerseInterpreter's rewriting component.</param>
     public VerseInterpreter(IRenderer renderer, IVerseSyntaxTreeBuilder syntaxTreeBuilder, IRewriter rewriter)
         => (_renderer, _syntaxTreeBuilder, _rewriter) = (renderer, syntaxTreeBuilder, rewriter);
 
+    /// <summary>
+    /// This method interprets the <paramref name="verseCode"/> using the <paramref name="wrapperFactory"/>.
+    /// First the ANTLR4 parse tree gets created, converted and desugared.
+    /// The parsed expression is placed in the wrapper from the factory and that itself inside the program.
+    /// Additionaly a <see cref="Stopwatch"/> provides time metrics for the rewriting.
+    /// </summary>
+    /// <param name="verseCode"><c>verseCode</c> is the Verse code to interpret.</param>
+    /// <param name="wrapperFactory"><c>wrapperFactory</c> is the factory from which the wrapper is retrieved.</param>
+    /// <returns>
+    /// <see cref="Fail"/> if either the number of syntax tree errors from ANTLR4 or the free variables of the program are not zero.
+    /// Otherwise the rewritten <see cref="Expression"/> with as many rewrite rules applied as possible.
+    /// </returns>
     public Expression Interpret(string verseCode, Func<Expression, Wrapper> wrapperFactory)
     {
         _renderer.DisplayMessage($"Input was:\n{verseCode}\n");
@@ -28,7 +70,7 @@ public class VerseInterpreter
         VerseParser parser = new(tokens) { BuildParseTree = true };
         VerseParser.ProgramContext programContext = parser.program();
         
-        if (parser.NumberOfSyntaxErrors > 0)
+        if (parser.NumberOfSyntaxErrors is not 0)
         {
             _renderer.DisplayMessage("Unable to parse verse program.");
 
