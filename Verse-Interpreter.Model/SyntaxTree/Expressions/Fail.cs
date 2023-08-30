67a7fa4f2df28e08bd655e5196b05f9983116577
Benson-sama <122638@fhwn.ac.at>
2023-08-30 10:54:55 +0200

    Add fail string representation to false?.

--- Verse-Interpreter.Model/SyntaxTree/Expressions/Fail.cs
+++ Verse-Interpreter.Model/SyntaxTree/Expressions/Fail.cs
@@ -7,5 +7,5 @@ public class Fail : Expression
     public override Fail DeepCopyButReplaceChoice(Choice choice, Expression newExpression)
         => new();
 
-    public override string ToString() => "fail";
+    public override string ToString() => "false?";
 }
