612440479b8ada412bf92a58d9fd0ce0a4808fd4
Benson-sama <122638@fhwn.ac.at>
2023-08-31 18:08:08 +0200

    Remove CLSCompliant attributes.

--- Verse-Interpreter.Model/ANTLR/VerseParser.cs
+++ Verse-Interpreter.Model/ANTLR/VerseParser.cs
@@ -31,7 +31,6 @@ using Antlr4.Runtime.Tree;
 using DFA = Antlr4.Runtime.Dfa.DFA;
 
 [System.CodeDom.Compiler.GeneratedCode("ANTLR", "4.13.0")]
-[System.CLSCompliant(false)]
 public partial class VerseParser : Parser {
 	protected static DFA[] decisionToDFA;
 	protected static PredictionContextCache sharedContextCache = new PredictionContextCache();
