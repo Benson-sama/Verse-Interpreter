174e2c751c37a5b621a144f5179e2f2cb92e5f07
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-09-04 22:18:23 +0200

    Add VerseParseException.

---
+++ Verse-Interpreter.Model/Build/VerseParseException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Verse_Interpreter.Model.Build;
+
+public class VerseParseException : Exception
+{
+    public VerseParseException()
+    {
+    }
+
+    public VerseParseException(string? message) : base(message)
+    {
+    }
+
+    public VerseParseException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected VerseParseException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
