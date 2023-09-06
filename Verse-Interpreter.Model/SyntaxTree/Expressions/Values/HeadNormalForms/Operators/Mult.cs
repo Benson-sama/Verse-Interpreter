42e4bf10b6c39a8df279aa52df4308b5763f36cc
Benson-sama <122638@fhwn.ac.at>
2023-09-06 11:12:58 +0200

    Document Verse operators.

--- Verse-Interpreter.Model/SyntaxTree/Expressions/Values/HeadNormalForms/Operators/Mult.cs
+++ Verse-Interpreter.Model/SyntaxTree/Expressions/Values/HeadNormalForms/Operators/Mult.cs
@@ -13,9 +13,19 @@ namespace Verse_Interpreter.Model.SyntaxTree.Expressions.Values.HeadNormalForms.
 /// </summary>
 public class Mult : Operator
 {
+    /// <summary>
+    /// This method accepts an <see cref="ISyntaxTreeNodeVisitor"/> in order to call it back.
+    /// </summary>
+    /// <param name="visitor"><c>visitor</c> is the <see cref="ISyntaxTreeNodeVisitor"/> to call back.</param>
     public override void Accept(ISyntaxTreeNodeVisitor visitor)
         => visitor.Visit(this);
 
+    /// <summary>
+    /// This method accepts an <see cref="ISyntaxTreeNodeVisitor{T}"/> in order to call it back.
+    /// </summary>
+    /// <typeparam name="T">The return type of this operation.</typeparam>
+    /// <param name="visitor"><c>visitor</c> is the <see cref="ISyntaxTreeNodeVisitor{T}"/> to call back.</param>
+    /// <returns>The result of the <c>visitor</c> callback.</returns>
     public override T Accept<T>(ISyntaxTreeNodeVisitor<T> visitor)
         => visitor.Visit(this);
 
