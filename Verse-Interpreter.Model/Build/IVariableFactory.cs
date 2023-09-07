6c95929577e3a35cb45e01a1216aa19ee9a59899
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-09-07 20:11:36 +0200

    Document IVariableFactory.

--- Verse-Interpreter.Model/Build/IVariableFactory.cs
+++ Verse-Interpreter.Model/Build/IVariableFactory.cs
@@ -1,10 +1,29 @@
-﻿using Verse_Interpreter.Model.SyntaxTree.Expressions.Values;
+﻿//-------------------------------------------------------------------
+// <copyright file="IVariableFactory.cs" company="FH Wiener Neustadt">
+//     Copyright (c) FH Wiener Neustadt. All rights reserved.
+// </copyright>
+// <author>Benjamin Bogner</author>
+// <summary>Contains the IVariableFactory class.</summary>
+//-------------------------------------------------------------------
+
+using Verse_Interpreter.Model.SyntaxTree.Expressions.Values;
 
 namespace Verse_Interpreter.Model.Build;
 
+/// <summary>
+/// Interface <see cref="IVariableFactory"/> serves as an interface for <see cref="Variable"/> factories.
+/// </summary>
 public interface IVariableFactory
 {
+    /// <summary>
+    /// Generates a fresh <see cref="Variable"/>.
+    /// </summary>
+    /// <returns>The generated <see cref="Variable"/>.</returns>
     Variable Next();
 
+    /// <summary>
+    /// Registers the <paramref name="name"/> as used to prevent generating with it.
+    /// </summary>
+    /// <param name="name"><c>name</c> is the name to register as used.</param>
     void RegisterUsedName(string name);
 }
