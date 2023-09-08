fac621ade9ac795bdd69609f915ae53d0ba0df21
Benjamin BOGNER <107477496+Benson-sama@users.noreply.github.com>
2023-09-08 15:38:04 +0200

    Document Rewrite.

--- Verse-Interpreter.Model/Rewrite/IRewriter.cs
+++ Verse-Interpreter.Model/Rewrite/IRewriter.cs
@@ -1,10 +1,26 @@
-﻿using Verse_Interpreter.Model.SyntaxTree;
+﻿//-------------------------------------------------------------
+// <copyright file="IRewriter.cs" company="FH Wiener Neustadt">
+//     Copyright (c) FH Wiener Neustadt. All rights reserved.
+// </copyright>
+// <author>Benjamin Bogner</author>
+// <summary>Contains the IRewriter interface.</summary>
+//-------------------------------------------------------------
+
+using Verse_Interpreter.Model.SyntaxTree;
 using Verse_Interpreter.Model.SyntaxTree.Expressions;
-using Verse_Interpreter.Model.SyntaxTree.Expressions.Values;
 
 namespace Verse_Interpreter.Model.Rewrite;
 
+/// <summary>
+/// Interface <see cref="IRewriter"/> serves as an interface for classes that
+/// handle the "execution" of a <see cref="VerseProgram"/> by rewriting it.
+/// </summary>
 public interface IRewriter
 {
+    /// <summary>
+    /// This method rewrites the <paramref name="verseProgram"/> to a single <see cref="Expression"/>.
+    /// </summary>
+    /// <param name="verseProgram"><c>verseProgram</c> is the <see cref="VerseProgram"/> to rewrite.</param>
+    /// <returns>The <see cref="Expression"/> representing the result of the <see cref="VerseProgram"/>.</returns>
     Expression Rewrite(VerseProgram verseProgram);
 }
--- Verse-Interpreter.Model/Rewrite/RewriteRuleAttribute.cs
+++ Verse-Interpreter.Model/Rewrite/RewriteRuleAttribute.cs
@@ -1,5 +1,17 @@
-﻿namespace Verse_Interpreter.Model.Rewrite;
+﻿//------------------------------------------------------------------------
+// <copyright file="RewriteRuleAttribute.cs" company="FH Wiener Neustadt">
+//     Copyright (c) FH Wiener Neustadt. All rights reserved.
+// </copyright>
+// <author>Benjamin Bogner</author>
+// <summary>Contains the RewriteRuleAttribute class.</summary>
+//------------------------------------------------------------------------
 
+namespace Verse_Interpreter.Model.Rewrite;
+
+/// <summary>
+/// Class <see cref="RewriteRuleAttribute"/> serves as an <see cref="Attribute"/> for
+/// marking methods as rewrite rules.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class RewriteRuleAttribute : Attribute
 {
