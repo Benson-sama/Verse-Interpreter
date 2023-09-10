eb6316dc266ba8afe95fe45f10ba00ff407e2842
Benson-sama <122638@fhwn.ac.at>
2023-09-10 21:32:19 +0200

    Document SyntaxTreeNode.

--- Verse-Interpreter.Model/SyntaxTree/SyntaxTreeNode.cs
+++ Verse-Interpreter.Model/SyntaxTree/SyntaxTreeNode.cs
@@ -1,13 +1,16 @@
-﻿//--------------------------------------------------------------
-// <copyright file=".cs" company="FH Wiener Neustadt">
+﻿//------------------------------------------------------------------
+// <copyright file="SyntaxTreeNode.cs" company="FH Wiener Neustadt">
 //     Copyright (c) FH Wiener Neustadt. All rights reserved.
 // </copyright>
 // <author>Benjamin Bogner</author>
-// <summary>Contains the  class.</summary>
-//--------------------------------------------------------------
+// <summary>Contains the SyntaxTreeNode class.</summary>
+//------------------------------------------------------------------
 
 namespace Verse_Interpreter.Model.SyntaxTree;
 
+/// <summary>
+/// Class <see cref="SyntaxTreeNode"/> serves as a base class for nodes in Verse (Calculus) syntax trees.
+/// </summary>
 public abstract class SyntaxTreeNode
 {
 }
