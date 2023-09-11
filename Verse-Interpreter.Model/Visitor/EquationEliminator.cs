9553531309d6be6f2dbddea6553ea3780e9872de
Benson-sama <122638@fhwn.ac.at>
2023-09-11 18:23:26 +0200

    Document EquationEliminator.

--- Verse-Interpreter.Model/Visitor/EquationEliminator.cs
+++ Verse-Interpreter.Model/Visitor/EquationEliminator.cs
@@ -1,4 +1,12 @@
-﻿using Verse_Interpreter.Model.SyntaxTree;
+﻿//----------------------------------------------------------------------
+// <copyright file="EquationEliminator.cs" company="FH Wiener Neustadt">
+//     Copyright (c) FH Wiener Neustadt. All rights reserved.
+// </copyright>
+// <author>Benjamin Bogner</author>
+// <summary>Contains the EquationEliminator class.</summary>
+//----------------------------------------------------------------------
+
+using Verse_Interpreter.Model.SyntaxTree;
 using Verse_Interpreter.Model.SyntaxTree.Expressions;
 using Verse_Interpreter.Model.SyntaxTree.Expressions.Equations;
 using Verse_Interpreter.Model.SyntaxTree.Expressions.Values;
@@ -8,34 +16,78 @@ using Verse_Interpreter.Model.SyntaxTree.Expressions.Wrappers;
 
 namespace Verse_Interpreter.Model.Visitor;
 
+/// <summary>
+/// Class <see cref="EquationEliminator"/> serves as an <see cref="ISyntaxTreeNodeVisitor"/>
+/// that replaces an <see cref="Eqe"/> containing a specific <see cref="Equation"/> with its child <see cref="Expression"/>.
+/// Its search pattern when eliminating is based on an "Execution Context" including an additional <see cref="Exists"/>,
+/// therefore leaving many <c>Visit</c> methods empty.
+/// </summary>
 public class EquationEliminator : ISyntaxTreeNodeVisitor
 {
+    /// <summary>
+    /// Field <c>_targetEqe</c> represents the <see cref="Eqe"/> that is holding the <see cref="Equation"/> to eliminate.
+    /// </summary>
     private readonly Eqe _targetEqe;
 
+    /// <summary>
+    /// Initialises a new instance of the <see cref="EquationEliminator"/> class.
+    /// </summary>
+    /// <param name="targetEqe"><c>targetEqe</c> represents the <see cref="Eqe"/> that is holding the <see cref="Equation"/> to eliminate.</param>
     public EquationEliminator(Eqe targetEqe) => _targetEqe = targetEqe;
 
+    /// <summary>
+    /// Property <c>TargetEqe</c> represents the <see cref="Eqe"/> that is holding the <see cref="Equation"/> to eliminate.
+    /// </summary>
     public Eqe TargetEqe => _targetEqe;
 
+    /// <summary>
+    /// This method eliminates the <see cref="Equation"/> occuring in the <c>TargetEqe</c>
+    /// by traversing through the given <paramref name="expression"/>.
+    /// </summary>
+    /// <param name="expression"><c>expression</c> represents the <see cref="Expression"/> where elimination happens.</param>
     public void EliminateEquationIn(Expression expression) => expression.Accept(this);
 
-    public void Visit(Variable variable) { }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(Variable _) { }
 
-    public void Visit(Integer integer) { }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(Integer _) { }
 
-    public void Visit(VerseString verseString) { }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(VerseString _) { }
 
-    public void Visit(Operator verseOperator) { }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(Operator _) { }
 
-    public void Visit(VerseTuple verseTuple) { }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(VerseTuple _) { }
 
-    public void Visit(Lambda lambda)
-    {
-        if (lambda.E == TargetEqe)
-            lambda.E = TargetEqe.E;
-        else
-            lambda.E.Accept(this);
-    }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(Lambda _) { }
 
+    /// <summary>
+    /// This method tries to locate the <c>TargetEqe</c> in the <paramref name="equation"/>
+    /// in order to eliminate its <see cref="Equation"/>.
+    /// </summary>
+    /// <param name="equation"><c>equation</c> represents the <see cref="Expression"/> where elimination happens.</param>
     public void Visit(Equation equation)
     {
         if (equation.E == TargetEqe)
@@ -44,6 +96,11 @@ public class EquationEliminator : ISyntaxTreeNodeVisitor
             equation.E.Accept(this);
     }
 
+    /// <summary>
+    /// This method tries to locate the <c>TargetEqe</c> in the <paramref name="eqe"/>
+    /// in order to eliminate its <see cref="Equation"/>.
+    /// </summary>
+    /// <param name="eqe"><c>eqe</c> represents the <see cref="Expression"/> where elimination happens.</param>
     public void Visit(Eqe eqe)
     {
         if (eqe.Eq == TargetEqe)
@@ -57,6 +114,11 @@ public class EquationEliminator : ISyntaxTreeNodeVisitor
             eqe.E.Accept(this);
     }
 
+    /// <summary>
+    /// This method tries to locate the <c>TargetEqe</c> in the <paramref name="exists"/>
+    /// in order to eliminate its <see cref="Equation"/>.
+    /// </summary>
+    /// <param name="exists"><c>exists</c> represents the <see cref="Expression"/> where elimination happens.</param>
     public void Visit(Exists exists)
     {
         if (exists.E == TargetEqe)
@@ -65,32 +127,33 @@ public class EquationEliminator : ISyntaxTreeNodeVisitor
             exists.E.Accept(this);
     }
 
-    public void Visit(Fail fail) { }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(Fail _) { }
 
-    public void Visit(Choice choice)
-    {
-        if (choice.E1 == TargetEqe)
-            choice.E1 = TargetEqe.E;
-        else
-            choice.E1.Accept(this);
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(Choice _) { }
 
-        if (choice.E2 == TargetEqe)
-            choice.E2 = TargetEqe.E;
-        else
-            choice.E2.Accept(this);
-    }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(Application _) { }
 
-    public void Visit(Application application) { }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(One _) { }
 
-    public void Visit(One one) => VisitWrapper(one);
-
-    public void Visit(All all) => VisitWrapper(all);
-
-    private void VisitWrapper(Wrapper wrapper)
-    {
-        if (wrapper.E == TargetEqe)
-            wrapper.E = TargetEqe.E;
-        else
-            wrapper.E.Accept(this);
-    }
+    /// <summary>
+    /// This method does nothing.
+    /// </summary>
+    /// <param name="_"><c>_</c> represents an unused argument.</param>
+    public void Visit(All _) { }
 }
